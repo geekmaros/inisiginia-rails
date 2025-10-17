f989a9aaa5f83914179dec926da9ce09fab7374a
Abdulrasaq Mustapha <razicruz@gmail.com>
2025-10-17 01:45:50 +0100

    feat: add testing

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get show" do
     get dashboard_show_url
     assert_response :success
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
